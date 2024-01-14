e6445bc7e69b3db1a81985b647b5e4333bb9fb51
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com.github.com>
2024-01-14 04:40:48 +0200

    Create products

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,6 +28,75 @@ function ModalPopup(){
 
 }
 
+function Products(){
+    const standingDesks  = [
+        {
+         name :"FlexiSpot E8 standing desk",
+         price:200
+        },
+
+        {
+            name : "Ikea skarsta/trotten standing desk",
+            price : 200
+        },
+
+        {
+            name :"Yo-Yo standing desk 90",
+            price:200
+        },
+
+        {
+            name :"Flytta 2 standing desk",
+            price:200
+        },
+
+        {
+            name :"Fully Jarvis bamboo standing desk",
+            price:200
+        },
+
+        {
+            name :"Herman Miller ratio sit-stand desk",
+            price:200
+        },
+
+        {
+            name :"Humanscale float standing desk",
+            price:200
+        },
+
+        {
+            name :"Flexispot children’s standing desk",
+            price:200
+        },
+
+        {
+            name :"Yo-Yo standing desk pro-3",
+            price:200
+        },
+
+        {
+            name :"HumbleWorks Stan 2 standing desk",
+            price:200
+        },
+
+        {
+            name :"Secretlab Magnus Pro",
+            price:200
+        },
+
+        {
+            name :"Autonomous SmartDesk Connect",
+            price:200
+        },
+        
+    ]
+
+    return {standingDesks};
+}
+
+function Cart(){}
+
 let modal = ModalPopup();
 modal.displayModal();
 modal.executeEvents()
